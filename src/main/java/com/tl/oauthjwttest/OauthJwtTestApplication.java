cacbc46ccec9e109c3761d89b028d592f1c14a4d
haily <49448330+melly8954@users.noreply.github.com>
2025-04-03 15:52:57 +0900

    init

---
+++ src/main/java/com/tl/oauthjwttest/OauthJwtTestApplication.java
@@ -0,0 +1,13 @@
+package com.tl.oauthjwttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthJwtTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthJwtTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tl/oauthjwttest/OauthJwtTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tl.oauthjwttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthJwtTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
