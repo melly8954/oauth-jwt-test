5a2c0eb853915a180d83df6aa53c9ddf33876be8
haily <49448330+melly8954@users.noreply.github.com>
2025-04-05 14:21:46 +0900

    시큐리티 업데이트

--- src/main/java/com/tl/oauthjwttest/global/config/SecurityConfig.java
+++ src/main/java/com/tl/oauthjwttest/global/config/SecurityConfig.java
@@ -13,30 +13,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        //csrf disable
+        // csrf disable
         http
                 .csrf((auth) -> auth.disable());
-
-        //From 로그인 방식 disable
+        // From 로그인 방식 disable
         http
                 .formLogin((auth) -> auth.disable());
-
-        //HTTP Basic 인증 방식 disable
+        // HTTP Basic 인증 방식 disable
         http
                 .httpBasic((auth) -> auth.disable());
-
-        //oauth2
+        // oauth2
         http
                 .oauth2Login(Customizer.withDefaults());
-
-        //경로별 인가 작업
+        // 경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated());
-
-        //세션 설정 : STATELESS
+        // 세션 설정 : STATELESS
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
